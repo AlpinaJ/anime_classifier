0047a9a7ead533cbf11cefce23d1978ca91733f4
julia953197 <julia953197@yandex.ru>
2021-11-29 11:48:38 +0300

    Styles changed

--- back/endpoints.py
+++ back/endpoints.py
@@ -13,29 +13,23 @@ app = FastAPI()
 templates = Jinja2Templates(directory="../templates")
 app.mount("/front", StaticFiles(directory="../front"), name="front")
 
+# Change the path to your folders
 model = Model(model_path="C:/Users/julia/PycharmProjects/anime_classifier/back/resnet_model",
               classes_path="C:/Users/julia/PycharmProjects/anime_classifier/back/class_names.txt")
 
-@app.post("/classify")
-async def face_endpoint(image: UploadFile = File(...)):
-    """
-    Обрабатываем фото пользователя
-    """
-    print(f"--------------- New request /face -------------")
 
-    # читаем переданную в параметрах картинку
+@app.post("/classify")
+async def classify(image: UploadFile = File(...)):
+
     print("Reading input pic")
     contents = await image.read()
     nparr = np.fromstring(contents, np.uint8)
     input_img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-    # сохраняем входную картинку
     print("Classifying input pic")
-    # if not cv2.imwrite("test.png", input_img):
-    #     print(f"ERROR on imwrite")
-    #     raise Exception("bad imwrite happened")
     res = cv2.resize(input_img, dsize=(128, 128), interpolation=cv2.INTER_CUBIC)
     label = model.predict(res)
+
     print("Classifying done")
     return {"label": label}
 
